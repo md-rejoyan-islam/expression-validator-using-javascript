a807c1a1476fb638afee4b4f6a1fb4c16d1bc58e
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-11-13 20:29:32 +0600

    form added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 const submitForm = document.getElementById("form-submit");
+const error = document.getElementById("error");
 
 // form submit event listener
 submitForm.addEventListener("submit", (e) => {
@@ -7,6 +8,13 @@ submitForm.addEventListener("submit", (e) => {
   const { expressionType, expression } = Object.fromEntries(formData);
   console.log(expressionType, expression);
   if (expressionType === "none" || !expression) {
+    return showMessage("error", "Please fill the form");
+  }
+  const result = checkExpression(expressionType, expression);
+  if (result) {
+    showMessage("success", "Expression is correct");
+  } else {
+    showMessage("error", "Expression is incorrect");
   }
 });
 
@@ -28,4 +36,32 @@ function checkExpression(type, value) {
     case "url":
       return expressionPattern.url.test(value);
   }
+}
+
+// error and success messages
+function showMessage(type, message) {
+  error.classList.remove("hidden");
+  // message show
+  error.querySelector("#errorMessage").textContent = message;
+  if (type === "error") {
+    error.classList.add("bg-red-500");
+  } else if (type === "success") {
+    error.classList.add("bg-green-500");
+  } else if (type === "error") {
+    error.classList.add("bg-red-500");
+  }
+
+  // remove message after 5 seconds
+  setTimeout(() => {
+    error.classList.add("hidden");
+    error.classList.remove("bg-red-500");
+    error.classList.remove("bg-green-500");
+  }, 5000);
+}
+
+// message hide
+function messageHide() {
+  error.classList.add("hidden");
+  error.classList.remove("bg-red-500");
+  error.classList.remove("bg-green-500");
 }
